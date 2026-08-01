ec3a2ad1248c1d9670d38ccb0179b8a77fbaa643
SUBRATA BAG <bagsubrata193@gmail.com>
2026-08-01 22:28:25 +0530

    Time: 0 ms (100%), Space: 42.9 MB (41.06%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean predictTheWinner(int[] nums) {
+         int n = nums.length;
+        int[][] dp = new int[n][n];
+
+        // Base case
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = nums[i];
+        }
+
+        // Fill table
+        for (int len = 2; len <= n; len++) {
+
+            for (int i = 0; i <= n - len; i++) {
+
+                int j = i + len - 1;
+
+                int left = nums[i] - dp[i + 1][j];
+                int right = nums[j] - dp[i][j - 1];
+
+                dp[i][j] = Math.max(left, right);
+            }
+        }
+
+        return dp[0][n - 1] >= 0;
+        
+    }
+}
