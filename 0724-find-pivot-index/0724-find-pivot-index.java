00990c1dd1e73bd9692fd1cf3d3645561cd39cf1
SUBRATA BAG <bagsubrata193@gmail.com>
2026-08-26 09:05:42 +0530

    Time: 1 ms (97.53%), Space: 47.4 MB (40.85%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int pivotIndex(int[] arr) {
+        int r = 0 ;
+        for(int i= 0 ; i<arr.length; i++){
+            int ele = arr[i];
+            r += ele;
+        }
+        int l = 0;
+        for(int i = 0 ; i< arr.length; i++){
+            r -= arr[i];
+            if(r == l){
+                return i;
+            }
+            l+=arr[i];
+
+        }
+        return -1;
+        
+        
+    }
+}
