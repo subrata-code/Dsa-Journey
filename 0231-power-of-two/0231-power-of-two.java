8952c276e460edabe926bbec4f9e795669a385f0
SUBRATA BAG <bagsubrata193@gmail.com>
2026-08-27 22:30:06 +0530

    Time: 1 ms (96.63%), Space: 42.6 MB (36.59%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,14 +1,15 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if (n == 0){
+        if(n == 0){
             return false;
         }
-         while (n%2 == 0){
+        while(n%2 == 0){
             n = n/2;
-        }if (n == 1){
-            return true;
+
         }
-        else{
+        if(n == 1){
+            return true;
+        }else{
             return false;
         }
 
