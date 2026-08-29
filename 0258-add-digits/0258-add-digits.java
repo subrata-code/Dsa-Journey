6b5033ae9e03d799f768b027ee3938bffe02080d
SUBRATA BAG <bagsubrata193@gmail.com>
2026-08-29 19:04:43 +0530

    Time: 1 ms (98.08%), Space: 42.6 MB (63.1%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int addDigits(int num) {
+        if(num<10) return num;
+
+        while(num >= 10){
+            int ans = 0;
+            while(num != 0){
+                ans += num % 10;
+                num /= 10; 
+            }
+            
+            num = ans;
+        }
+        return num;
+        
+    }
+}
