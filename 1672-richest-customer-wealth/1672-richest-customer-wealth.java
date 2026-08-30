ffb7adaaa1bf94fdfc83c2d6b398cd8c25408df9
SUBRATA BAG <bagsubrata193@gmail.com>
2026-08-30 18:27:18 +0530

    Time: 0 ms (100%), Space: 44.4 MB (73.92%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int result = 0;
+        for (int i =0 ; i < accounts.length ; i++){
+            int sum = 0 ;
+            for(int j = 0 ; j < accounts[i].length ; j++){
+                sum = sum + accounts[i][j];
+            }
+            result = Math.max(result, sum);
+        }
+        return result;
+        
+    }
+}
